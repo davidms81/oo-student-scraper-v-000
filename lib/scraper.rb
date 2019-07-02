b22bcfccf82b5cf95397ed19715c14e7098b2f41
David Schoenholtz <dmschoenholtz@gmail.com>
2019-07-02 00:55:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,8 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     scraped_students = []
+    doc.css("div.roster-cards-container").each do |card|
+    end
   end
 
   def self.scrape_profile_page(profile_url)
